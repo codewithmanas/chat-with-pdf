56f9bcbc298d04ed09a50c95f03500b0bb727972
Manas Ranjan Adha <manas865873@gmail.com>
2024-07-25 23:02:34 +0530

    add Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,40 @@
+import { SignedIn, UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
 import React from 'react'
+import { Button } from './ui/button'
+import { FilePlus2 } from 'lucide-react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="flex justify-between bg-white shadow-sm p-5 border-b">
+      <Link
+        href='/dashboard'
+        className="text-2xl font-inter"
+      >
+        Chat to <span className="text-amber-600">PDF</span>
+      </Link>
+
+      <SignedIn>
+        <div className="flex items-center space-x-2">
+          <Button asChild variant="link" className="hidden md:flex">
+                <Link href="/dashboard/upgrade">Pricing</Link>
+          </Button>
+
+          <Button asChild variant="outline">
+                <Link href="/dashboard">My Documents</Link>
+          </Button>
+
+          <Button asChild variant="outline" className="border-amber-600">
+                <Link href="/dashboard/upload">
+                  <FilePlus2 className="text-amber-600" />
+                </Link>
+          </Button>
+
+          {/* Upgrade Button */}
+          <UserButton />
+        </div>
+      </SignedIn>
+    </div>
   )
 }
 
