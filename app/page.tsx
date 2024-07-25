c9797232b6529872af65e58b86321cd70140ab54
Manas Ranjan Adha <manas865873@gmail.com>
2024-07-25 22:17:53 +0530

    add layout for dashboard route

--- app/page.tsx
+++ app/page.tsx
@@ -28,38 +28,38 @@ const features = [
     icon: BrainCogIcon,
   },
   {
-    name: "Chat Memorization",
-    description: "Our intelligent chatbot remembers previous interactions, providing a seamless and personalized experience.",
-    icon: BrainCogIcon,
+    name: "Interactive PDF Viewer",
+    description: "Engage with your PDFs like never before using our intuitive and interactive viewer.",
+    icon: EyeIcon,
   },
   {
-    name: "Chat Memorization",
-    description: "Our intelligent chatbot remembers previous interactions, providing a seamless and personalized experience.",
-    icon: BrainCogIcon,
+    name: "Cloud Backup",
+    description: "Rest assured knowing your documents are safely backed up on the cloud, protected from loss or damage.",
+    icon: ServerCogIcon,
   },
   {
-    name: "Chat Memorization",
-    description: "Our intelligent chatbot remembers previous interactions, providing a seamless and personalized experience.",
-    icon: BrainCogIcon,
+    name: "Responsive Across Devices",
+    description: "Access and chat with PDFs seamlessly on any device, whether it's your desktop, tablet, or smartphone.",
+    icon: MonitorSmartphoneIcon,
   },
 ]
 
 export default function Home() {
   return (
-    <main className="flex-1 overflow-scroll p-2 lg:p-5 bg-gradient-to-bl from-white to-indigo-600">
+    <main className="flex-1 overflow-scroll p-2 lg:p-5 bg-gradient-to-bl from-white to-amber-600">
       <div className="bg-white py-24 sm:py-32 rounded-md drop-shadow-xl">
           <div className="flex flex-col justify-center items-center mx-auto max-w-7xl px-6 lg:px-8">
               <div className="mx-auto max-w-3xl sm:text-center">
-                <h2 className="text-base font-semibold leading-7 text-indigo-600">Your Interactive Document Companion</h2>
+                <h2 className="text-base font-semibold leading-7 text-amber-600 font-merriweather">Your Interactive Document Companion</h2>
 
-                <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl">Transform Your PDFs into Interactive Conversations</p>
+                <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl font-old-standard-tt">Transform Your PDFs into Interactive Conversations</p>
 
-                <p className="mt-6 text-lg leading-8 text-gray-600">
+                <p className="mt-6 text-lg leading-8 text-gray-600 font-merriweather">
                   Introducing{" "}
-                  <span className="font-bold text-indigo-600">Chat with PDF.</span>
+                  <span className="font-bold text-amber-600">Chat with PDF.</span>
                   <br />
                   <br /> Upload your document, and our chatbot will answer questions, summarize conent, and answer all your Qs. Ideal for everyone, 
-                  <span className="text-indigo-600">
+                  <span className="text-amber-600">
                     Chat with PDF
                   </span>{" "}
                   turns static documents into{" "}
@@ -68,7 +68,7 @@ export default function Home() {
                 </p>
               </div>
 
-              <Button asChild className="mt-10">
+              <Button asChild className="mt-10 font-inter">
                 <Link href="/dashboard">Get Started</Link>
               </Button>
           </div>
@@ -83,9 +83,8 @@ export default function Home() {
                 className="mb-[-0%] rounded-xl shadow-2xl ring-1 ring-gray-900/10"
               />
 
-            <div aria-hidden="true" className="relative">
+            <div aria-hidden="true" className="relative ">
               <div className="absolute bottom-0 -inset-x-32 bg-gradient-to-t from-white/95 pt-[5%]" />
-
             </div>
 
             </div>
@@ -98,11 +97,11 @@ export default function Home() {
                           <dt className="inline font-semibold text-gray-900">
                               <feature.icon
                                 aria-hidden="true"
-                                className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+                                className="absolute left-1 top-1 h-5 w-5 text-amber-600"
                               />
                           </dt>
 
-                          <dd>{feature.description}</dd>
+                          <dd className="font-merriweather">{feature.description}</dd>
                     </div>
                   ))}
               </dl>
