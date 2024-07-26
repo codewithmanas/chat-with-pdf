81bfe1a33432f78e0952c066cc06c75464f9c123
Manas Ranjan Adha <manas865873@gmail.com>
2024-07-26 23:35:45 +0530

    implement react-dropzone UI

---
+++ app/dashboard/upload/page.tsx
@@ -0,0 +1,13 @@
+import FileUploader from '@/components/FileUploader'
+import React from 'react'
+
+const UploadPage = () => {
+  return (
+    <div>
+
+        <FileUploader />
+    </div>
+  )
+}
+
+export default UploadPage
